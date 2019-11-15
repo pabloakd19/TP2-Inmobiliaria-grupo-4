a7d4266e67a00b040eabfb1b665b042b3f740b7d
fer-puchalski <57409469+fer-puchalski@users.noreply.github.com>
2019-11-14 22:54:43 -0300

    Alta vivienda a medio camino

--- TP2-Inmobiliaria-grupo-4/alta_viviendas.cs
+++ TP2-Inmobiliaria-grupo-4/alta_viviendas.cs
@@ -27,5 +27,27 @@ namespace TP2_Inmobiliaria_grupo_4
             cn.Open();
 
         }
+
+        private void Btn_agregar_viv_Click(object sender, EventArgs e)
+        {
+            //para dar de alta wacheen
+            cn.Open();
+            SqlCommand com = new SqlCommand("insert into Viviendas" +
+                "(superficie, habitaciones, baños, precio, terraza, jardin, piscina, garage, promo_id)" +
+                "values(@su, @ha ,@ba ,@pre, @te, @ja, @pi, @ga, @pro)", cn);
+
+            com.Parameters.AddWithValue("@su", textBox1.Text);
+            com.Parameters.AddWithValue("@ha", textBox2.Text);
+            com.Parameters.AddWithValue("@ba", textBox3.Text);
+            com.Parameters.AddWithValue("@pre", textBox4.Text);
+            com.Parameters.AddWithValue("@te", checkBox1.Checked);
+            com.Parameters.AddWithValue("@pi", checkBox2.Checked);
+            com.Parameters.AddWithValue("@ja", checkBox4.Checked);
+            com.Parameters.AddWithValue("@ga", checkBox3.Checked);
+
+            com.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Alta exitosa");
+        }
     }
 }
