755f1eca3f8bf298e68938a5d10c88d992df161e
Pablo Tártalo <pabloakd19@gmail.com>
2019-11-18 21:21:16 -0300

    consulta3

---
+++ TP2-Inmobiliaria-grupo-4/Consulta-Promociones.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TP2_Inmobiliaria_grupo_4
+{
+    partial class Consulta_Promociones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_promoid = new System.Windows.Forms.TextBox();
+            this.btn_consulta3 = new System.Windows.Forms.Button();
+            this.dataGrid_cons_prom = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid_cons_prom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ingrese el código de la Promoción: ";
+            // 
+            // txt_promoid
+            // 
+            this.txt_promoid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.txt_promoid.Location = new System.Drawing.Point(440, 21);
+            this.txt_promoid.Name = "txt_promoid";
+            this.txt_promoid.Size = new System.Drawing.Size(135, 31);
+            this.txt_promoid.TabIndex = 11;
+            // 
+            // btn_consulta3
+            // 
+            this.btn_consulta3.Location = new System.Drawing.Point(221, 74);
+            this.btn_consulta3.Name = "btn_consulta3";
+            this.btn_consulta3.Size = new System.Drawing.Size(262, 34);
+            this.btn_consulta3.TabIndex = 12;
+            this.btn_consulta3.Text = "Consultar Promoción";
+            this.btn_consulta3.UseVisualStyleBackColor = true;
+            this.btn_consulta3.Click += new System.EventHandler(this.btn_consulta3_Click);
+            // 
+            // dataGrid_cons_prom
+            // 
+            this.dataGrid_cons_prom.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid_cons_prom.Location = new System.Drawing.Point(12, 114);
+            this.dataGrid_cons_prom.Name = "dataGrid_cons_prom";
+            this.dataGrid_cons_prom.Size = new System.Drawing.Size(816, 286);
+            this.dataGrid_cons_prom.TabIndex = 13;
+            // 
+            // Consulta_Promociones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 412);
+            this.Controls.Add(this.dataGrid_cons_prom);
+            this.Controls.Add(this.btn_consulta3);
+            this.Controls.Add(this.txt_promoid);
+            this.Controls.Add(this.label1);
+            this.Name = "Consulta_Promociones";
+            this.Text = "Consulta Promociones";
+            this.Load += new System.EventHandler(this.Consulta_Promociones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid_cons_prom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_promoid;
+        private System.Windows.Forms.Button btn_consulta3;
+        private System.Windows.Forms.DataGridView dataGrid_cons_prom;
+    }
+}
---
+++ TP2-Inmobiliaria-grupo-4/Consulta-Promociones.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace TP2_Inmobiliaria_grupo_4
+{
+    public partial class Consulta_Promociones : Form
+    {
+        SqlConnection cn;
+
+        public Consulta_Promociones()
+        {
+            InitializeComponent();
+        }
+
+        private void Consulta_Promociones_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(Class1.Cadena());
+        }
+
+        private void btn_consulta3_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+
+            DataTable dt2 = new DataTable();
+            SqlDataAdapter da2;
+
+            da2 = new SqlDataAdapter
+               ("select promociones.promo_id, nombre, poblacion, " +
+               "vivienda_id, superficie, nro_habitaciones, nro_baños, " +
+               "precio, terraza, jardin, piscina, garage from viviendas, " +
+               "promociones where promociones.promo_id = " + txt_promoid.Text +
+               "and viviendas.promo_id = promociones.promo_id", cn);
+
+
+            da2.Fill(dt2);
+
+            dataGrid_cons_prom.DataSource = dt2;
+
+
+            cn.Close();
+        }
+    }
+}
