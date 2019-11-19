94533a27af6cff4e6124283325ef4b5b26adb192
Alcides Gabriel Barrios <alcidesgb@hotmail.com>
2019-11-19 17:21:36 -0300

    Se ordenan la conexiones del servidor de Datos

--- TP2-Inmobiliaria-grupo-4/Class1.cs
+++ TP2-Inmobiliaria-grupo-4/Class1.cs
@@ -11,22 +11,25 @@ namespace TP2_Inmobiliaria_grupo_4
     {
         public static string Cadena()
         {
-               return  "data source = 192.168.1.46;" + // servidor
-                      "initial catalog = Promociones_inmobiliarias;" + // base de datos
-                      "uid = sa; pwd = entrerios757";
+            //return  "data source = 192.168.1.46;" + // servidor
+            //        "initial catalog = Promociones_inmobiliarias;" + // base de datos
+            //        "uid = sa; pwd = entrerios757";
 
             // Pablo Tartalo
             // return @"data source = LAPTOP-BDOHKMHV;" +
-              
+
             // Otro Integrante 
             //   return @"data source = USUARIO-RMGTSE8\SQLEXPRESS;" +
-            
+
+            // Alcides Barrios 
+               return @"data source = IFTS24\SQLEXPRESS;" +
+
             // Federico
             //string cadena = @"data source = DESKTOP-VM0VUTF\SQLEXPRESS01;" +//nombre servidor
-            
+
             // DATOS GENERALES (para todos igual)
-             //                  " initial catalog = Promociones_inmobiliarias ;" + //nombre base de datos
-             //                " integrated security=sspi ";
+                            " initial catalog = Promociones_inmobiliarias ;" + //nombre base de datos
+                            " integrated security=sspi ";
         }
     }
 }
