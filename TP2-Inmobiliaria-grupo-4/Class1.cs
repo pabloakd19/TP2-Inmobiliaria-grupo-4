648274a066ee2e13a90ff7e22333e92728b2fdae
pabloakd19 <57194240+pabloakd19@users.noreply.github.com>
2019-11-19 13:00:44 -0300

    arreglando cagada en el class

--- TP2-Inmobiliaria-grupo-4/Class1.cs
+++ TP2-Inmobiliaria-grupo-4/Class1.cs
@@ -11,20 +11,22 @@ namespace TP2_Inmobiliaria_grupo_4
     {
         public static string Cadena()
         {
-              // return  "data source = 192.168.1.46;" + // servidor
-               //       "initial catalog = biblioteca;" + // base de datos
-               //       "uid = sa; pwd = entrerios757";
+               return  "data source = 192.168.1.46;" + // servidor
+                      "initial catalog = Promociones_inmobiliarias;" + // base de datos
+                      "uid = sa; pwd = entrerios757";
 
             // Pablo Tartalo
-             return @"data source = LAPTOP-BDOHKMHV;" +
+            // return @"data source = LAPTOP-BDOHKMHV;" +
               
             // Otro Integrante 
             //   return @"data source = USUARIO-RMGTSE8\SQLEXPRESS;" +
             
             // Federico
             //string cadena = @"data source = DESKTOP-VM0VUTF\SQLEXPRESS01;" +//nombre servidor
-                               " initial catalog = Promociones_inmobiliarias ;" + //nombre base de datos
-                             " integrated security=sspi ";
+            
+            // DATOS GENERALES (para todos igual)
+             //                  " initial catalog = Promociones_inmobiliarias ;" + //nombre base de datos
+             //                " integrated security=sspi ";
         }
     }
 }
